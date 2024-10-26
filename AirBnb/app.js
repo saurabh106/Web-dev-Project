748f3ffa76075bf4d3db68bba92b84d8ad6d8e91
saurabh phadtare <saurabhphadtare901@gmail.com>
2024-10-26 10:43:39 +0530

    Created a edit , update and delete route now started styling using Ejs-mate for template created a Public/css folder for static css in page created Layouts in that a boilerplate , includes folder a layouts small segment of code also remove the all html from ejs file else using Layouts

--- AirBnb/app.js
+++ AirBnb/app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listings")
 const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -18,9 +20,12 @@ async function main() {
 }
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { 
     res.send("I am a Rott");
 });
 
@@ -31,22 +36,21 @@ app.get("/Listings", async (req, res) => {
 });
 
 //New Route
-app.get("/listings/new",(req,res)=>{
+app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs")
-    });
-
-    
-//show route
-app.get("/Listings/:id", async (req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
 });
 
+//show route
+app.get("/Listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+});
 
 //Create Route
 /* Get location image etc from req.body.listing
 new Listing() create a new model instance of it by giving a (listing) you can put newListing(new.ejs) to our model and from that they directly save to our main model 
+And Also created a normal try catch to display the error
 */
 app.post("/listings", async (req, res) => {
     const newListing = new Listing(req.body.listing);
@@ -59,10 +63,33 @@ app.post("/listings", async (req, res) => {
     }
 });
 
+//Edit route
+//  When you click on Edit btn(having btn in show.ejs )the form will open to edit content
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+});
 
+//Update Route
+//After fill the form click on edit btn then the update will done redirect to that page
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing }); //{ ...req.body.listing } uses the spread syntax to unpack the listing object from req.body. 
+    //This assumes req.body contains a listing object with the updated properties for the listing, such as title, description, or price
+    res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async(req,res)=>{
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
+ 
 
 // app.get("/testListing",(req,res)=>{
-
 // })
 
 app.listen(8080, () => {
