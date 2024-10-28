0608fb8f373a2e0175b5508217e0a36adf9c3ce9
saurabh phadtare <saurabhphadtare901@gmail.com>
2024-10-28 10:20:09 +0530

    Working on Styling !

--- AirBnb/models/listings.js
+++ AirBnb/models/listings.js
@@ -2,19 +2,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ListingSchema = new Schema({
-    title:{
-        type:String,
-        required:true
+    title: {
+        type: String,
+        required: true
     },
-    description:String,
-    price:Number,
-    location:String,
-    image:{
-      filename:String,
-      url: String,
+    description: String,
+    price: Number,
+    location: String,
+    image: {
+        filename: {
+            type: String,
+            default: "default.jpg" // Optional default filename if needed
+        },
+        url: {
+            type: String,
+            default: "https://unsplash.com/photos/the-sun-is-shining-over-the-water-and-rocks-CIuhewxFdxU", // URL of your default image
+            set: v => v === "" ? "https://unsplash.com/photos/the-sun-is-shining-over-the-water-and-rocks-CIuhewxFdxU" : v
+        }
     },
-    country:String
+    country: String
 });
 
-const Listing = mongoose.model("Listing",ListingSchema);
+const Listing = mongoose.model("Listing", ListingSchema);
 module.exports = Listing;
