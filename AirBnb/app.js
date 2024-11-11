bd5b3a647acde6a44019396a84b565a80f9a7f93
saurabh phadtare <saurabhphadtare901@gmail.com>
2024-11-11 21:56:19 +0530

    Created a validation for Review route by using Cilent(form) / Server (Joi Schema,Schema Validation function and middlewares)

--- AirBnb/app.js
+++ AirBnb/app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync");
 const ExpressError = require("./utils/ExpressError");
-const { listingSchema } = require("./schema");
+const { listingSchema,reviewSchema } = require("./schema");
 const Review = require("./models/review");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
     res.send("Nothing to see here !!");
 });
 
-
+// This is for create route 
 const validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body);
     if (error) {
@@ -43,6 +43,17 @@ const validateListing = (req, res, next) => {
         next();
     }
 };
+
+const validateReview= (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);  // Pass error to ExpressError
+    } else {
+        next();
+    }
+};
+
 
 
 //index route
@@ -105,7 +116,8 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
 
 // Review
 // Post Route
-app.post("/listings/:id/reviews" , async (req,res)=>{
+app.post("/listings/:id/reviews" ,validateReview,wrapAsync
+    (async (req, res) => {
   let listing  = await Listing.findById(req.params.id);  // find Id 
   let newReview = new Review(req.body.review); // Create a newReview
 
@@ -117,7 +129,7 @@ app.post("/listings/:id/reviews" , async (req,res)=>{
 //   console.log("New Review Saved"); 
 //   res.send("New Review Saved");
 res.redirect(`/listings/${listing._id}`); 
-});
+}));
 
 
 
