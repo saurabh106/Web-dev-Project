b82e6592314df699863519b205f639187998f2a1
saurabh phadtare <saurabhphadtare901@gmail.com>
2024-11-20 08:43:38 +0530

    Removed fake user creation endpoint and added user signup route with registration functionality

--- AirBnb/app.js
+++ AirBnb/app.js
@@ -89,17 +89,17 @@ app.use((req, res, next) => {
 });
 
 
-// To create a fakeUser adn added in db
-app.get("/demouser", async (req, res) => {
-    let fakeUser = new User({
-        email: "student@gmail.com",
-        username: "student"
-    });
-    //register is a static method that use to save it in this para(user,"password") already check this is user / pw is unique or not
-    // passport-local-mongoose use there own hash function pbkdf2 algo
-    let registeredUser = await User.register(fakeUser, "helloworld");
-    res.send(registeredUser);
-});
+// // To create a fakeUser adn added in db
+// app.get("/demouser", async (req, res) => {
+//     let fakeUser = new User({
+//         email: "student@gmail.com",
+//         username: "student"
+//     });
+//     //register is a static method that use to save it in this para(user,"password") already check this is user / pw is unique or not
+//     // passport-local-mongoose use there own hash function pbkdf2 algo
+//     let registeredUser = await User.register(fakeUser, "helloworld");
+//     res.send(registeredUser);
+// });
 
 
 // For restructing code this coming from listing.js in routes folder 
--- AirBnb/routes/user.js
+++ AirBnb/routes/user.js
@@ -3,12 +3,26 @@
 
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
+const wrapAsync = require("../utils/wrapAsync");
 
-router.get("/signup", (req,res)=>{
+router.get("/signup", (req, res) => {
     res.render("users/signup.ejs")
 })
 
-
+router.post("/signup", wrapAsync(async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ email, username });
+        const registeredUser = await User.register(newUser, password);
+        console.log(registeredUser);
+        req.flash("sucess", "Welcome to wanderlust!");
+        res.redirect("/listings");
+    } catch (e) {
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+}));
 
 
 module.exports = router;
