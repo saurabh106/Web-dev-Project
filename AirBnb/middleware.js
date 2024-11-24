6b8bdbab75a5e595a26daebf410b002548576d77
saurabh phadtare <saurabhphadtare901@gmail.com>
2024-11-25 00:01:44 +0530

    Added redirectUrl to session and flash error message in isLoggedIn middleware

--- AirBnb/middleware.js
+++ AirBnb/middleware.js
@@ -2,6 +2,10 @@
 
 module.exports.isLoggedIn = (req,res,next)=>{
     if(!req.isAuthenticated()){
+        // redirectUrl save -> 
+        // If user want to access any path  - then we need to login first then - when login they redirect to the same path that are they accessing before login
+       
+        req.session.redirectUrl = req.originalUrl;
         req.flash("error", "You must be logged in to create a new listing");
        return  res.redirect("/login");
     }
